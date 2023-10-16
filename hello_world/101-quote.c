26e69ff114628b2020641d0b3c4ba79f9e7ba327
MitzaelPitre <7636@holbertonstudents.com>
2023-10-15 17:44:17 -0700

    Task

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Unix
+ *
+ * Return: Always 1
+ */
+
+int main (void)
+
+{	
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-10/n", 59);
+	return (1);
+}
