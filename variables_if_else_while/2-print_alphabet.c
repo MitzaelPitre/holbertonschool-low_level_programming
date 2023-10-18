a36e5ee079e950069468e523cf17460b205c7da4
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-18 00:08:23 -0400

    Coding

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* Learning putchar */
+/**
+ * main - Using putchart
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+		}
+	putchar('\n');
+	return (0);
+}
