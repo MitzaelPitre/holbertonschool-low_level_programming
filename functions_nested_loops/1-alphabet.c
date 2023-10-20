ac7e05266e6a23554cfa2c2c511081ce49ebfabd
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-20 08:07:24 -0400

    Coding

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* Making task one */
+
+/**
+ * print_alphabet - make the alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c= 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
