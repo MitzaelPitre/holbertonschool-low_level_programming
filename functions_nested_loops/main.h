00ec25c9ce1756ab1cab21cbbf8f365e5cacc937
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-21 12:17:21 -0400

    Coding

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: Mitzael Pitre
- * Desc: File with declarations for all functions
- */
-
 int _putchar(char c)
 void print_alphabet(void);
 void print_alphabet_x10(void);
