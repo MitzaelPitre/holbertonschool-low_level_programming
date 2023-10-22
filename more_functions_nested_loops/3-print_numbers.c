640fe6ac2f00db97c509ca420c69295f075b67fe
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-22 15:31:06 -0400

    Coding

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/* Task number three */
+
+/**
+ * print numbers - print the numbers since 0 to 9
+ * Return: The numbers since 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 0; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
