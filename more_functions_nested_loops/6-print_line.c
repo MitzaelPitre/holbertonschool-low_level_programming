94e7f218ca9282075fecc9d923d647db19495f10
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-22 16:23:55 -0400

    Coding

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 /* Task number six */
 
 /**
- * print_line: Draws a straight line according to parameter
+ * print_line - Draws a straight line according to parameter
  * @n: the numbers of lines to draw
  * Return: empty
  */
@@ -12,6 +12,7 @@ void print_line(int n)
 
 {
 	int x;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
