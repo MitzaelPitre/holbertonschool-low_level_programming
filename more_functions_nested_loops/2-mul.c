561da2ffd522f4bc3e27e6a7d1ef20670a7143fa
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-22 15:19:20 -0400

    Coding

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/* Task number two */
+
+/**
+ * mul - Multiplies two integers
+ * @a - the first number to multiply
+ * @b - the second number to multiply with first number
+ * Return: The result of multiplication between a param and b param
+ */
+
+int mul (int a, int b)
+{
+	return (a * b);
+}
