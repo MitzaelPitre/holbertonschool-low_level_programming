a9948bb5a4a23761de870fd1bc2828f54dcaf063
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-22 15:23:21 -0400

    Coding

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,8 +4,8 @@
 
 /**
  * mul - Multiplies two integers
- * @a - the first number to multiply
- * @b - the second number to multiply with first number
+ * @a: the first number to multiply
+ * @b: the second number to multiply with first number
  * Return: The result of multiplication between a param and b param
  */
 
