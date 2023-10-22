82f853144728a9c3b475f04ad03ee639ab54cb29
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-22 14:39:47 -0400

    Coding

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,17 +3,17 @@
 /* Task one */
 
 /**
- * _isdigit - Checks for a digit 
- * @c: input
- * Return: 1 if c is a digit, 0 otherwise
- */
+* _isdigit - Checks for a digit.
+* @c: input
+* Return: 1 if c is a digit, 0 otherwise
+*/
 
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
-		return(1);
+		return (1);
 	}
 	else
-		return(0);
+		return (0);
 }
