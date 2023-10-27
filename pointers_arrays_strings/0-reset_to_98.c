d59d040ccc67b00728bdf2e1da7095b0231c4613
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-26 23:55:30 -0400

    Coding

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/* Task zero */
+
+/**
+ * reset_to_98 - TAke a pointer to an int and update the value
+ * @n: An integer input pointer
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
