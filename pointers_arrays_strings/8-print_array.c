c8b2d1aa620837b4a35071f72e977f253e80d28c
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-28 10:49:31 -0400

    Coding

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,10 @@ void print_array(int *a, int n)
 {
 	int m;
 
-	for (m = 0; m < l; m++)
+	for (m = 0; m < n; m++)
 	{
 	printf("%d", a[m]);
-	if (m != (l - 1))
+	if (m != (n - 1))
 	{
 	printf(", ");
 	}
