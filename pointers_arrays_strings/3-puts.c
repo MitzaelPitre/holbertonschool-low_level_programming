b1b8270c65c99dcd91abb099d8832cfc4b726612
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-28 00:31:57 -0400

    Coding

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -5,14 +5,14 @@
 /**
  * _puts - prints a string.
  * @str: the string to print
- * Return: 0 if successfull
+ * Return: void
  */
 
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++))
 	{
-		_putchar(*str);
+		puts(*str);
 	}
 	_putchar('\n');
 }
