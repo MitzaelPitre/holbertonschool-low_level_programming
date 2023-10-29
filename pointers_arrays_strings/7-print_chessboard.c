5efb0e3c3ad60d01a64dea051cd924f983b814c0
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-10-29 17:52:50 -0400

    Coding

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* task six */
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array
+ * Return: always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, n:
+
+		for (i = 0; i < 8; i++)
+		{
+			for (n = 0; n < 8; n++)
+				_putchar(a[i][n]);
+			_putchar('\n');
+		}
+}
