4f9f9ca60d62bf1075abd21830ec074b8bed1cab
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-12 17:58:27 -0400

    Coding

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -32,6 +32,6 @@ char *_strdup(char *str)
 		a++;
 	}
 
-	s[a] = '/0';
+	s[a] = '\0';
 	return (s);
 }
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,6 +7,7 @@
  * str_concat - Concatenates two string of any size
  * @s1: the first string to concatenate
  * @s2: the second string to concatenate
+ *
  * Return: the two string concatenated
  */
 
@@ -17,18 +18,15 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[i])
 		i++;
-
 	while (s2[j])
 		j++;
 
 	l = i + j;
-	s = malloc ((sizeof(char) * l) + 1);
+	s = malloc((sizeof(char) * l) + 1);
 
 	if (s == NULL)
 		return (NULL);
@@ -39,7 +37,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		if (k <= i)
 			s[k] = s1[k];
-
 		if (k >= i)
 		{
 			s[k] = s2[j];
