38b519365cee482e9e3701efaf3500c70a3d203f
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-12 18:12:37 -0400

    Coding

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+/* task three */
+
+/**
+ * alloc_grid - return a pointer to a dimensional array
+ * @width: ...
+ * @height: ...
+ * Return: NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, k, l;
+	int **a;
+
+	if (width <= 0 || heigth <= 0)
+		return (NULL);
+
+	a = malloc(sizeof(int *) * height);
+
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+
+		if (a[i] == NULL)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				free(a[j]);
+			}
+
+			free(a);
+			return (NULL);
+		}
+	}
+
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; 1 < width; l++)
+		{
+			a[k][l] = 0;
+		}
+	}
+
+	return (a);
+}
