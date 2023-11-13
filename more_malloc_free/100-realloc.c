8be39414a8690c64bfeb87174b32bb3c27b649f7
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-12 20:10:35 -0400

    Coding

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i, n = new_size;
 	char *oldp = ptr;
 
-	if (ptr = NULL)
+	if (ptr == NULL)
 	{
 		p = malloc(new_size);
 		return (p);
