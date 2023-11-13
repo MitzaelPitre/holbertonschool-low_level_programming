7ac58bc97cf6166b2d84bec0186f0cdb8b6fd05e
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-12 20:12:07 -0400

    Coding

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -7,7 +7,7 @@
  * _realloc - rallocates a memory block using malloc
  * @ptr: pointer to the memory previously allocated
  * @old_size: is the size, in bytes, of the allocated space for ptr
- * @new size: the new size, in bytes of the new memory block
+ * @new_size: the new size, in bytes of the new memory block
  * Return: pointer allocate new size memory or NULL
  */
 
