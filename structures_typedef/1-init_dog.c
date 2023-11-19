b10fc5cf3c7d4ab018e771ad5137190ba6ada9f3
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-18 21:01:33 -0400

    Coding

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,13 +4,13 @@
 
 /**
  * init_dog - Initializes a dog structure
- * @d: A dog structure
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * @d: A dog structure.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
  * Return: nothing
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
