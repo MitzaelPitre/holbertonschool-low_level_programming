b94b1e55b3c851744a12ca721841d4ee8f399d7b
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-19 19:32:10 -0400

    Coding

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * op_add - Returns the sum of two numbers
  * @a: The first number
@@ -59,11 +65,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
