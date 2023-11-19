872d542d631a880c074134e903452f01d3926021
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-19 18:22:08 -0400

    Coding

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/* task one */
+
+/**
+ * array_iterator - execute a function given as a parameter on each element
+ * of an array.
+ * @size: how many elem to print
+ * @array: array
+ * @action: pointer to print in regular or hex
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
