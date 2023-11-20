901d64b42f4a55f4bed6197f9b1af44f31ea2d4e
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-11-19 21:16:37 -0400

    Coding

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 				case 's':
 					str = va_arg(list, char *);
 					if (!str)
-						str = "(nil)';
+						str = "(nil)";
 					printf("%s%s", sep, str);
 					break;
 
