32649d6328395657a7d3a5ab054ffdbd55d5bac2
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 19:59:19 -0400

    Coding

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,30 +10,30 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	dlistint_t *temp;
+    dlistint_t *new_node;
+    dlistint_t *temp;
 
-	(void)temp;
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new = malloc(sizeof(dlistint_t));
+    new_node->n = n;
+    new_node->next = NULL;
 
-	if (new == NULL)
-		return (NULL);
+    temp = *head;
 
-	new->n = n;
-	new->next = NULL;
-	temp = *head;
-	if (*head == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = new;
-	}
-	return (*head);
+    if (temp == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    return (*head);
 }
