d3a8d87544f42b36023b1e7456a47bc8c474fb07
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 19:40:17 -0400

    Coding

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,9 +5,9 @@
  * add_dnodeint - add a new node at the beginning of a linked
  * @head: head of a list
  * @n: n element
- * Return: address of the new element. NULL it is failed
+ * Return: address of the new element, NULL if it failed
  */
-
+	n
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	else
 	{
-		while (temp->next 1= NULL)
+		while (temp->next != NULL)
 		{
 			temp = temp->next;
 		}
