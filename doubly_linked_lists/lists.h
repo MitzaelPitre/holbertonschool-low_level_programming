5d5f250e9cd658052fdf35f2aec09cc5a81a15d2
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 19:27:04 -0400

    Coding

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,36 +1,35 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_
+#define LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * struct dlistint_s- singly linked list
+ * struct dlistint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ * for Holberton project
  */
-
 typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *next;
-}
-dlistint_t;
+} dlistint_t;
 
 /**
  * struct dlistp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
- * Descriptions: singly linked list of pointers
+ *
+ * Description: singly linked list of pointers
  */
-
 typedef struct dlistp_s
 {
 	void *p;
 	struct dlistp_s *next;
-}
-dlistp_t;
+} dlistp_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -46,6 +45,6 @@ int delete_nodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *reverse_dlistint(dlistint_t **head);
 size_t print_dlistint_safe(const dlistint_t *head);
 size_t free_dlistint_safe(dlistint_t **h);
-dlistint_t *find_dlistint_loop(dlistint_t *head);
+dlistint_t *find_listint_loop(dlistint_t *head);
 
 #endif
