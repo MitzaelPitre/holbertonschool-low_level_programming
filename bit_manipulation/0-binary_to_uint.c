8a30a9107028191a91e169ae0c2fe8f8b180c95d
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 21:58:08 -0400

    Coding

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -16,15 +16,15 @@ unsigned int binary_to_uint(const char *b)
 	if (*b == '\0')
 		return (0);
 
-	for (len = 0; b[len];)
+	for (len = 0; *b[len];)
 		len++;
 
 	for (len -= 1; len >= 0; len--)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b[len] != '0' && *b[len] != '1')
 			return (0);
 
-		num += (b[len] - '0') * mult;
+		num += (*b[len] - '0') * mult;
 		mult *= 2;
 	}
 	return (num);
