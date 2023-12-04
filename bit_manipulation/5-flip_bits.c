e8fd87351f9be66083924892a10bbe97990d1d6e
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 21:56:01 -0400

    Coding

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -2,8 +2,8 @@
 /* last task */
 
 /**
- * flip_bits - Counts the number of bits needed to be flipped to get from one number
- * to another
+ * flip_bits - Counts the number of bits needed to be flipped
+ * to get from one number to another
  * @n: The number
  * @m: The number to flip n to
  * Return: The necessary number of bits to flip to get from n to m
