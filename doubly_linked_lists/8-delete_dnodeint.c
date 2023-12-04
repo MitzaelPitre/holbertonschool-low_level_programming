d04a867757eb65f75f4b08f6d861153858045c8d
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 23:44:36 -0400

    Coding

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -19,20 +19,27 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		if (*head)
+			(*head)->prev = NULL;
 		free(copy);
 		return (1);
 	}
 
 	for (node = 0; node < (index - 1); node++)
 	{
-		if (copy->next == NULL)
-			return (-1);
-
 		copy = copy->next;
 	}
 
-	tmp = copy->next;
-	copy->next = tmp->next;
-	free(tmp);
+	if (copy = NULL)
+		return (-1);
+
+
+	tmp = copy->prev;
+	tmp->next = copy->next;
+	
+	if (copy->next != NULL)
+		copy->next->prev = tmp;
+	
+	free(copy);
 	return (1);
 }
