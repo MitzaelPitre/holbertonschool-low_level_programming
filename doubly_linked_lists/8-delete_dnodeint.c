bc3358b5ff59b522620d2b3ad456eee5b5284be8
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 23:37:04 -0400

    Coding

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	listint_t *tmp, *copy = *head;
+	dlistint_t *tmp, *copy = *head;
 	unsigned int node;
 
 	if (copy == NULL)
