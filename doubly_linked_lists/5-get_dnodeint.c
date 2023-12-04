a896bc00245c8428f07ae66d30461fc99217dab4
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2023-12-03 20:51:27 -0400

    Coding

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,13 +2,13 @@
 /* task five */
 
 /**
- * get_nodeint_at_index - returns the nth node of a linked list
+ * get_dnodeint_at_index - returns the nth node of a linked list
  * @head: head of a list
  * @index: index of the node
  * Return: nth node. If node does not exist, returns NULL.
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,12 +2,12 @@
 /* task six */
 
 /**
- * sum_listint - returns the sum of all the data (n) of a linked list
+ * sum_dlistint - returns the sum of all the data (n) of a linked list
  * @head: head of a list
  * Return: sum of all the data (n).
  */
 
-int sum_listint(listint_t *head)
+int sum_dlistint(dlistint_t *head)
 {
 	int sum;
 
